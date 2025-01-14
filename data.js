6d29c02972edf253603f3e3a126a88acddad6fca
Loktaru <nidalargio11@gmail.com>
2025-01-15 02:59:06 +0400

    Upload

---
+++ data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall",
+      "name": "hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008907043156465733,
+          "pitch": 0.40604009957113263,
+          "rotation": 0,
+          "target": "5-bedroom"
+        },
+        {
+          "yaw": 1.4719847897067089,
+          "pitch": 0.5045622018374978,
+          "rotation": 0,
+          "target": "1-guest-room-1"
+        },
+        {
+          "yaw": -1.4735243181932702,
+          "pitch": 0.39122774135497984,
+          "rotation": 0,
+          "target": "4-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-guest-room-1",
+      "name": "guest room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.636071836039946,
+          "pitch": 0.49899756791137584,
+          "rotation": 0,
+          "target": "3-guestroom-2"
+        },
+        {
+          "yaw": 1.8373181826995317,
+          "pitch": 0.4992661306824484,
+          "rotation": 0,
+          "target": "2-bed-room-2"
+        },
+        {
+          "yaw": -1.2906750341872772,
+          "pitch": 0.43736431529563724,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bed-room-2",
+      "name": "bed room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9720454799700047,
+          "pitch": 0.2186484304967493,
+          "rotation": 0,
+          "target": "3-guestroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-guestroom-2",
+      "name": "guestroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3281879479842722,
+          "pitch": 0.4581185635817331,
+          "rotation": 0,
+          "target": "1-guest-room-1"
+        },
+        {
+          "yaw": -0.9669793886391957,
+          "pitch": -0.08706692379660197,
+          "rotation": 0,
+          "target": "0-hall"
+        },
+        {
+          "yaw": -2.31541773160194,
+          "pitch": 0.10495053115153752,
+          "rotation": 0,
+          "target": "2-bed-room-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom",
+      "name": "bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8298855259254658,
+          "pitch": 0.24714299628069014,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom",
+      "name": "bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.007742438079628,
+          "pitch": 0.46477156810435716,
+          "rotation": 0,
+          "target": "0-hall"
+        },
+        {
+          "yaw": 2.9235375214847386,
+          "pitch": 0.06216903093792325,
+          "rotation": 0,
+          "target": "1-guest-room-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
